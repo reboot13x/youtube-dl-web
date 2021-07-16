d74369b9bf4ede94aa88846817a09d502c9df622
Krutik Raut <64907570+reboot13x@users.noreply.github.com>
2021-07-16 11:04:08 +0530

    Update app.js

--- app.js
+++ app.js
@@ -99,8 +99,8 @@ app.get('/ytdl', function (req, res) {
     }) 
 
 app.listen(process.env.PORT || 3000, function(){
-        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-      });
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
     
 
 
